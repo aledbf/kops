836f4478e45c62a8f20d2511ad1fd3b7935e0767
Justin Santa Barbara <justin@fathomdb.com>
2017-02-15 10:57:05 -0500

    Add socat to CoreOS
    We build a statically linked version and distribute it with kops.
    
    Note that our version of socat does not include libssl, but kubernetes
    does not use it anyway.

---
+++ images/utils-builder/extract.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -ex
+
+rm -f /utils.tar.gz
+rm -rf /utils
+
+mkdir -p /utils
+cp /socat/socat-*/debian/socat/usr/bin/socat /utils/socat
+#(sha1sum /utils/socat | cut -d' ' -f1) > /utils/socat.sha1
+
+tar cvfz /utils.tar.gz /utils
+
+cp /utils.tar.gz /dist/utils.tar.gz
+(sha1sum /dist/utils.tar.gz | cut -d' ' -f1) > /dist/utils.tar.gz.sha1
+
